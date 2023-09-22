b5147458157894ba58c0bc78afa93aec2f5e804d
agbenn <hockeystar614@yahoo.com>
2023-09-22 19:38:19 +0200

    add docstrings

--- function_proj.py
+++ function_proj.py
@@ -4,19 +4,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def model_1():
-    pop = 1000
-    base_infected = 10
-    days = 0
-    infection_rate = 0.05
+def model_1(pop = 1000, base_infected = 10, infection_rate = 0.05):
+    """
+    function to project the infection rate of a disease
+    :param int pop: total population
+    :param int base_infected: initial infected population
+    :param float infection_rate: infection rate of the disease
+
+    """
+    
     inf_dict = {'date':[0],'infected':[10]}
+    days = 0
 
     inf_df = pd.DataFrame(inf_dict)
 
     while base_infected < 1000:
         infected = math.ceil((infection_rate*3)*base_infected*(pop - base_infected)/(pop))
         base_infected = base_infected + infected
-        days = days + 1
+        days += 1
         # Append values to dictionary
         inf_dict['date'].append(days)
         inf_dict['infected'].append(base_infected)
@@ -26,13 +31,16 @@ def model_1():
 
 ## Model vaccination
 
-def model_2():
-    pop = 1000
-    base_infected = 10
+def model_2(pop = 1000, base_infected = 10, infection_rate = 0.05, vax_rate=0.01):
+    """
+    function to project the infection rate of a disease when a vaccine is introduced
+    :param int pop: total population
+    :param int base_infected: initial infected population
+    :param float infection_rate: infection rate of the disease
+    :param float vax_rate: effective rate of the vaccine
+    """
     days = 0
     inf_dict = {'date':[0],'infected':[10]}
-    vax_rate = 0.01
-    infection_rate = 0.05
 
 
     while base_infected < 1000:
@@ -51,20 +59,27 @@ def model_2():
 
     return pd.DataFrame(inf_dict)
 
-def sir_model():
-    pop = 1000
+def sir_model(pop = 1000, base_infected = 10, infection_rate = 0.05, recovery_rate=0.9, death_rate=0.1):
+    """
+    function to project the infection rate of a disease when deaths and recoveries are accounted for
+    :param int pop: total population
+    :param int base_infected: initial infected population
+    :param float infection_rate: infection rate of the disease
+    :param float recovery_rate: recovery rate of the disease
+    :param float death_rate: death rate of the disease
+    """
     days = 7
     inf_diction = {'date':[0,1,2,3,4,5,6],'recovers':[0,0,0,0,0,0,10],'dead':[0,0,0,0,0,0,1]}
     recovers = 9
-    base = 10
     dead = 1
+
     while recovers <= 1000 :
-        infected = math.ceil((0.05*3)*base*(pop - base)/(pop))
-        recovery = math.ceil((0.05*3)*base*(pop - base)/(pop)*0.90)
+        infected = math.ceil((infection_rate*3)*base_infected*(pop - base_infected)/(pop))
+        recovery = math.ceil((infection_rate*3)*base_infected*(pop - base_infected)/(pop)*0.90)
         recovers = recovers + recovery
-        deads  = math.ceil((0.05*3)*base*(pop - base)/(pop)*0.10)
+        deads  = math.ceil((infection_rate*3)*base_infected*(pop - base_infected)/(pop)*0.10)
         dead = dead + deads
-        base = base + infected - deads
+        base_infected = base_infected + infected - deads
         days = days + 1
         inf_diction['date'].append(days)
         inf_diction['recovers'].append(recovers)
